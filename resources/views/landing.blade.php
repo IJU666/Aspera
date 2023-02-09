61dab618a8ccc6c1002d00179c1c6f2221a52ee0
IJU666 <fauzimaulanabandung2020@gmail.com>
2023-02-09 11:06:03 +0700

    landing

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,47 @@
+@extends('layout.layout')
+@include('layout.navbar')
+@section('content')
+<div class="col-lg-12 py-5" style="background-color: #F5F5F5;">
+    <div class="col-lg-10 mx-auto row">
+        <div class="col-lg-5 align-self-center mx-auto">
+            <h5 class="fw-semibold">A S P E R A</h5>
+            <h5 class="fw-semibold">Layanan Aspirasi</h5>
+            <h5 class="fw-semibold">dan Pengaduan Masyarakat</h5>
+            <p>Laporkan Keluhan atau Aspirasi Kepada Lembaga Pemerintah Berwenang</p>
+        </div>
+        <div class="col-lg-5 float-end mx-auto">
+            <img src="img/konten1.png" class="img-fluid" width="350" alt="">
+        </div>
+    </div>
+</div>
+<div class="container mt-4">
+    <h5 class="fw-semibold text-center">Alur Aspera</h5>
+    <div class="border-top border-3 col-lg-1 mx-auto"></div>
+    <div class="mt-4">
+        <img src="img/alur.png" class="img-fluid" width="" alt="">
+    </div>
+<div class="mt-5">
+    <h5 class="fw-semibold text-center">Sampaikan laporan anda</h5>
+    <div class="col-lg-1 border-top border-3 mx-auto "></div>
+</div>
+<div class="col-lg-4 mt-5 mx-auto">
+    <p><small>Pilih Klasifikasi Laporan</small></p>
+    <form action="" method="post">
+        <div class="row">
+        <div class="form-check d-inline col-lg-4 p-2 rounded-2" style="background-color: #F5F5F5;">
+            <input class="form-check-input mx-auto" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
+            <label class="form-check-label ms-1" for="exampleRadios1">
+              Pengaduan
+            </label>
+          </div>
+          <div class="form-check d-inline col-lg-4 ms-2 p-2 justify-content-center rounded-2" style="background-color: #F5F5F5;">
+            <input class="form-check-input mx-auto" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
+            <label class="form-check-label ms-1" for="exampleRadios2">
+              Aspirasi
+            </label>
+          </div>
+        </div>
+    </form>
+</div>
+</div>
+@endsection
---
+++ resources/views/layout/navbar.blade.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-expand-lg bg-body-light">
+    <div class="container-fluid p-0 container col-10">
+      <a class="navbar-brand p-0" href="#">
+        <img src="img/logo.png" class="" alt="" width="50">
+        </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav col-lg-12">
+          <li class="nav-item">
+            <a class="nav-link active fw-semibold" aria-current="page" href="#">Tentang Aspera</a>
+          </li>
+          <li class="nav-item col-lg-8 me-5">
+            <a class="nav-link fw-semibold" href="#">Pengaduan</a>
+          </li>
+          <a href="" class="btn text-center fw-semibold">Masuk</a>
+          <a href="" class="btn btn-primary fw-semibold">Daftar</a>
+        </ul>
+      </div>
+    </div>
+  </nav>
