3c9cc5f9af25e0833c85ec2e50d7ba5cf4a86ab4
IJU666 <fauzimaulanabandung2020@gmail.com>
2023-02-09 19:50:45 +0700

    landing page

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -29,29 +29,44 @@
     <form action="" method="post">
         <div class="row col-lg-12 mx-auto">
         <div class="form-check d-inline col-lg-5 p-2 rounded-2" style="background-color: #F5F5F5;">
-            <input class="form-check-input mx-auto" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
+            <input class="form-check-input mx-auto" type="radio" name="pengaduan" id="exampleRadios1" value="option1" checked>
             <label class="form-check-label ms-1" for="exampleRadios1">
               Pengaduan
             </label>
           </div>
           <div class="form-check d-inline col-lg-5 ms-2 p-2 justify-content-center rounded-2" style="background-color: #F5F5F5;">
-            <input class="form-check-input mx-auto" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
+            <input class="form-check-input mx-auto" type="radio" name="aspirasi" id="exampleRadios2" value="option2">
             <label class="form-check-label ms-1" for="exampleRadios2">
               Aspirasi
             </label>
           </div>
         </div>
         </div>
-        <div class="rounded-3 col-lg-12 mt-5 py-5" style="background-color: #F5F5F5;">
+        <div class="rounded-3 col-lg-10 mx-auto mt-5 pt-5 pb-3 shadow shadow-3" style="background-color: #F5F5F5;">
             <div class="form-floating col-lg-10 mx-auto mb-3">
-                <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea" style="height: 200px;"></textarea>
+                <textarea class="form-control" name="isi" placeholder="Leave a comment here" id="floatingTextarea" style="height: 200px;" required></textarea>
                 <label for="floatingTextarea" class="fw-semibold text-secondary">Ketik isi laporan anda*</label>
-                <div class="form-floating col-lg-5 mt-3">
-                    <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea" style="height: 150px;"></textarea>
-                    <label for="floatingTextarea" class="fw-semibold text-secondary">Masukan alamat lengkap kejadian*</label>
+                <div class="row">
+                <div class="form-floating col-lg-6 mt-3">
+                    <textarea class="form-control" name="alamat" placeholder="Leave a comment here" id="floatingTextarea" style="height: 120px;" required></textarea>
+                    <label for="floatingTextarea" class=" ms-2 fw-semibold text-secondary">Masukan alamat lengkap kejadian*</label>
                   </div>
+                  <div class="mt-3 col-lg-6">
+                    <input type="date" name="tglpengaduan" id="" class="form-control" required>
+                    <label for="lampiran" class="form-text mt-2 text-secondary">Upload lampiran</label>
+                    <input type="file" name="lampiran" id="lampiran" class="form-control" required>
+                  </div>
+                </div>
+                <div class="col-lg-4 mt-4 border mx-auto">
+                <button type="submit" name="kirim" class="btn btn-outline-primary col-lg-12">Kirim</button>
+            </div>
               </div>
         </div>
     </form>
 </div>
+<div class="mt-5 py-4" style="background-color: #00AFF5;">
+    <h4 class="fw-semibold text-center text-light">Jumlah pengaduan saat ini</h4>
+    <h1 class="text-center text-light">100</h1>
+</div>
 @endsection
+
